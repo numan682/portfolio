ba79e399d9b8acb62905157cb2cd4033ea6b215e
Jahidul Hasan Numan <61766176+numan682@users.noreply.github.com>
2022-01-19 21:10:28 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+document.getElementById('facebook-link').href="https://facebook.com/jahidulhassan.numan";
+document.getElementById('github-link').href="https://github.com/numan682";
+document.getElementById('linkedin-link').href="https://www.linkedin.com/in/jahidul-hasan-numan-5b51771b0/";
+document.getElementById('instagram-link').href="https://www.instagram.com/alpha_x_numan/";
+document.getElementById('twitter-link').href="https://www.twitter.com";
